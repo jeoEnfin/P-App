d5d15e1e60ca2e7f98dc5e50b9a77b285f779261
jeothankachan270898 <jeothankachan270898@gmail.com>
2023-08-09 18:39:39 +0530

    app development shadcn ui istalled

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,7 +1,30 @@
+import AccountProfile from "@/components/forms/AccountProfile"
+import { currentUser } from "@clerk/nextjs";
+
 async function Page() {
+
+    const user = await currentUser();
+
+    // const userData = {
+    //     id: user?.id,
+    //     objectId: userInfo?._id,
+    //     username: userInfo?.username || user?.username,
+    //     name: userInfo?.name || user?.firstName || "",
+    //     bio: userInfo?.bio || "",
+    //     image: userInfo?.image || user?.imageUrl,
+    // }
+
     return (
-        <main>
-            <h1 className="head-text">Hello World! Am Onboarding</h1>
+        <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
+            <h1 className="head-text">Onboarding</h1>
+            <p className="mt-3 text-base-reguler text-light-2">
+                Complete your profile now to use P
+            </p>
+            <section className="mt-9 bg-dark-2 p-10">
+                <AccountProfile 
+                user 
+                btnTitle="Continue"/>
+            </section>
         </main>
     )
 }
